736421cee0986e1db454472993923d5ffdfb7b0e
Nurassyl Zekenov <nurassylzekenov@gmail.com>
2026-05-21 10:30:45 +0500

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        r = ""
+        for s in strs:
+            r += str(len(s)) + "@" + s
+        return r
+
+    def decode(self, s: str) -> List[str]:
+        res, i = [], 0
+
+        while i < len(s):
+            j = i
+            while s[j] != "@":
+                j += 1
+            l = int(s[i:j])
+            i = j + 1
+            j = i + l
+            res.append(s[i:j])
+            i = j
+        return res
